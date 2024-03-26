7955df899acc2dc291453508dd9d196b2d7535f3
Qringie <alex@blastup.io>
2024-03-26 20:24:51 +0000

    Add debug

--- .helm/prepare_values.sh
+++ .helm/prepare_values.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
 set -e
+set -x
 
 PRODUCTION_SECRETS_RAW=$(vault kv get -format json "kv/production")
-cat launchpad/values.yaml.tpl > launchpad/values.yaml
+cat ./launchpad/values.yaml.tpl > ./launchpad/values.yaml
 
 for k in $(jq -r '.data.data | keys | .[]' <<< $PRODUCTION_SECRETS_RAW); do
   v=$(jq -r ".data.data.$k" <<< $PRODUCTION_SECRETS_RAW)
-  echo "      $k: \"$v\"" >> launchpad/values.yaml
+  echo "      $k: \"$v\"" >> ./launchpad/values.yaml
 done
+
+cat ./launchpad/values.yaml

f274c6ff23fcab6a19d505f6ab442db731052f9c
greenxcat <green_cat@blastup.io>
2024-03-28 22:34:57 +0300

    router

--- app/router/v1/router.py
+++ app/router/v1/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from .project import router as project_router
+from .crypto import router as crypto_router
 
 router = APIRouter(prefix="/v1")
 router.include_router(project_router)
+router.include_router(crypto_router)

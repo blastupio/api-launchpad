b506a9cdf5856dfeac94157159f002cad77247c3
Qringie <alex@blastup.io>
2024-04-04 16:06:58 +0100

    Add proxy for onramp endpoints

--- app/router/v1/proxy.py
+++ app/router/v1/proxy.py
@@ -1,14 +1,16 @@
 import asyncio
 from typing import Union
+from uuid import UUID
 
 import httpx
-from fastapi import APIRouter, Path, Depends, Body
+from fastapi import APIRouter, Request, Path, Depends, Body
 from redis.asyncio import Redis
 
 from app.crud import LaunchpadProjectCrud
 from app.dependencies import get_launchpad_projects_crud, get_redis
 from app.models import LaunchpadProject
-from app.schema import ProjectDataResponse, ErrorResponse, AddressBalanceResponse, SaveTransactionResponse
+from app.schema import ProjectDataResponse, ErrorResponse, AddressBalanceResponse, SaveTransactionResponse, \
+    OnrampOrderResponseData, OnrampOrderRequest
 from app.utils import get_data_with_cache
 
 router = APIRouter(prefix="/proxy", tags=["proxy"])
@@ -18,6 +20,18 @@ async def fetch_data(api_url: str) -> dict:
     async with httpx.AsyncClient() as client:
         response = await client.get(api_url)
         return response.json()
+
+
+def get_ip_from_request(request: Request) -> str:
+    try:
+        ip = request.headers.get('cf-connecting-ip')
+        if not ip:
+            ip = request.headers.get('x-forwarded-for')
+        if not ip:
+            ip = request.client.host
+    except Exception as e:
+        raise RuntimeError("Cannot get ip from request: {}".format(e))
+    return ip
 
 
 @router.get("/{id_or_slug}/project-data", response_model=ProjectDataResponse | ErrorResponse)
@@ -86,11 +100,17 @@ async def get_balance(
 
 @router.post("/{id_or_slug}/transactions", response_model=SaveTransactionResponse | ErrorResponse)
 async def save_transaction(
+        request: Request,
         id_or_slug: Union[str, int],
         projects_crud: LaunchpadProjectCrud = Depends(get_launchpad_projects_crud),
         payload: dict = Body(embed=False)
 ):
     payload["source"] = "launchpad"
+    try:
+        payload["ip"] = get_ip_from_request(request)
+    except:
+        pass
+
     project: LaunchpadProject = await projects_crud.retrieve(id_or_slug=id_or_slug)
     base_url = project.base_proxy_url[0].base_url
 
@@ -102,3 +122,42 @@ async def save_transaction(
         return json_response
 
     return {"ok": True, "data": json_response.get("data")}
+
+
+@router.post("/{id_or_slug}/onramp/payment-link")
+async def get_onramp_payment_link(
+        request: Request,
+        id_or_slug: Union[str, int],
+        projects_crud: LaunchpadProjectCrud = Depends(get_launchpad_projects_crud),
+        payload: OnrampOrderRequest = Body(embed=False)
+):
+    payload_json = payload.model_dump()
+    try:
+        payload_json["ip"] = get_ip_from_request(request)
+    except:
+        pass
+    payload_json["source"] = "launchpad"
+    project: LaunchpadProject = await projects_crud.retrieve(id_or_slug=id_or_slug)
+    base_url = project.base_proxy_url[0].base_url
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(base_url + f"/onramp/payment-link", json=payload_json)
+        json_response = response.json()
+
+    return json_response
+
+
+@router.get("/{id_or_slug}/onramp/order/{order_id}", response_model=OnrampOrderResponseData | ErrorResponse)
+async def get_onramp_order_status(
+        id_or_slug: Union[str, int],
+        projects_crud: LaunchpadProjectCrud = Depends(get_launchpad_projects_crud),
+        order_id: UUID = Path()
+):
+    project: LaunchpadProject = await projects_crud.retrieve(id_or_slug=id_or_slug)
+    base_url = project.base_proxy_url[0].base_url
+
+    async with httpx.AsyncClient() as client:
+        response = await client.get(base_url + f"/onramp/order/{order_id}")
+        json_response = response.json()
+
+    return json_response

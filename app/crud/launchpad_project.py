c9aed55077b5ed29dc1927939099558d77cb1cbe
Marat K <marat@near.money>
2024-04-11 15:35:41 +0100

    loads olny projects with proxy link

--- app/crud/launchpad_project.py
+++ app/crud/launchpad_project.py
@@ -1,19 +1,20 @@
 from typing import Sequence, Union
 from sqlalchemy import select, or_
-from sqlalchemy.orm import selectinload
+from sqlalchemy.orm import selectinload, joinedload
 
 from app.base import BaseCrud
-from app.models import LaunchpadProject, StatusProject
+from app.models import LaunchpadProject, StatusProject, ProxyLink
 
 
 class LaunchpadProjectCrud(BaseCrud):
 
-    async def all(self, limit: int = 100, offset: int = 0, status: StatusProject = None) -> \
+    async def all_with_proxy(self, limit: int = 100, offset: int = 0, status: StatusProject = None) -> \
             Sequence[LaunchpadProject]:
         st = select(LaunchpadProject) \
             .options(selectinload(LaunchpadProject.profile_images)) \
             .options(selectinload(LaunchpadProject.links)) \
-            .options(selectinload(LaunchpadProject.proxy_link)) \
+            .options(joinedload(LaunchpadProject.proxy_link)) \
+            .where(ProxyLink.id != None) \
             .order_by(LaunchpadProject.created_at.asc()) \
             .limit(limit) \
             .offset(offset)

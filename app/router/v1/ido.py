68732dae8a782a7863ac6ad230e17349e185fadd
Nikita <shamaevn@blastup.io>
2024-04-18 16:10:28 +0300

    one env for LAUNCHPAD_CONTRACT_ADDRESS

--- app/router/v1/ido.py
+++ app/router/v1/ido.py
@@ -1,9 +1,12 @@
 import asyncio
+
+from fastapi import APIRouter, Path
+
 from app.dependencies import CryptoDep
-from app.env import IDO_SIGN_ACCOUNT_PRIVATE_KEY, LAUNCHPAD_CONTRACT_ADDRESS, LAUNCHPAD_CONTRACT_ADDRESS_TESTNET
+from app.env import IDO_SIGN_ACCOUNT_PRIVATE_KEY, LAUNCHPAD_CONTRACT_ADDRESS
 from app.schema import SignUserBalanceResponse
 from app.services.ido import generate_signature
-from fastapi import APIRouter, Path
+
 
 
 router = APIRouter(prefix="/ido", tags=["ido"])
@@ -16,7 +19,6 @@ async def sign_user_balance(
 ) -> SignUserBalanceResponse:
     chains = ("eth", "polygon", "bsc", "blast")
     chain_id = 81457 if crypto.environment == "mainnet" else 168587773
-    launchpad_contract_address = LAUNCHPAD_CONTRACT_ADDRESS if crypto.environment == "mainnet" else LAUNCHPAD_CONTRACT_ADDRESS_TESTNET
 
     _balances = await asyncio.gather(*[crypto.get_token_balance(chain, user_address) for chain in chains])
     balance = sum(balance for balance in _balances if isinstance(balance, int))
@@ -25,7 +27,7 @@ async def sign_user_balance(
         user_address=user_address,
         balance=balance,
         chain_id=chain_id,
-        launcpad_contract_address=launchpad_contract_address,
+        launcpad_contract_address=LAUNCHPAD_CONTRACT_ADDRESS,
         private_key=IDO_SIGN_ACCOUNT_PRIVATE_KEY,
     )
     return {"signature": signature}

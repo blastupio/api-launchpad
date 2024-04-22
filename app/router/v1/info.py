ac2fe9f67f6da915b08bb497f3f370bf82185e4d
0xniksh <167305260+0xniksh@users.noreply.github.com>
2024-04-22 20:09:52 +0300

    mock price methods (#29)

--- app/router/v1/info.py
+++ app/router/v1/info.py
@@ -1,8 +1,40 @@
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, Query
 
-from app.schema import TierInfoResponse, UserInfoResponse
+from app.schema import (
+    TierInfoResponse,
+    UserInfoResponse,
+    ChainId,
+    Address,
+    TokenInChain,
+    TokenPriceResponse,
+    Any2AnyPriceResponse,
+)
 
 router = APIRouter(prefix="/info", tags=["info"])
+
+
+@router.get("/token-price", response_model=TokenPriceResponse)
+async def get_token_price(
+    chain_id: int = Query(..., example=1),
+    token_addresses: str = Query(
+        description="comma-separated list of token addresses",
+        example="0xE1784da2b8F42C31Fb729E870A4A8064703555c2,0x0000000000000000000000000000000000000000",  # noqa
+    ),
+) -> TokenPriceResponse:
+    return {"price": {Address("0x0000000000000000000000000000000000000000"): 123.12}}
+
+
+@router.post("/any2any-price", response_model=Any2AnyPriceResponse)
+async def get_any2any_price_rate(
+    from_token: TokenInChain, to_tokens: list[TokenInChain]
+) -> Any2AnyPriceResponse:
+    return {
+        "rate": {
+            1: {
+                "0x0000000000000000000000000000000000000000": 123.12,
+            }
+        }
+    }
 
 
 @router.get("/user/{address}", response_model=UserInfoResponse)

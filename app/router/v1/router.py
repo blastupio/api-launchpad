75f47a1a36a25957f12ab774d4ef4b11d86fa8a4
Nikita <shamaevn@blastup.io>
2024-04-22 16:44:43 +0300

    mock tiers info methods

--- app/router/v1/router.py
+++ app/router/v1/router.py
@@ -4,9 +4,11 @@ from .project import router as project_router
 from .crypto import router as crypto_router
 from .proxy import router as proxy_router
 from .ido import router as ido_router
+from .info import router as info_router
 
 router = APIRouter(prefix="/v1")
 router.include_router(project_router)
 router.include_router(crypto_router)
 router.include_router(proxy_router)
 router.include_router(ido_router)
+router.include_router(info_router)

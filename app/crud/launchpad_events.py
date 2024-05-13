01528f93aaaa4182dea3acd7a0c5d4fcc30917a2
Nikita <shamaevn@blastup.io>
2024-05-13 10:18:42 +0300

    save results to google sheet, run periodic task

--- app/crud/launchpad_events.py
+++ app/crud/launchpad_events.py
@@ -1,4 +1,6 @@
+from sqlalchemy import select, Row, Sequence
 from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.ext.asyncio import AsyncConnection
 
 from app.base import BaseCrud
 from app.models import LaunchpadContractEvents
@@ -6,6 +8,11 @@ from app.schema import CreateLaunchpadEvent
 
 
 class LaunchpadContractEventsCrud(BaseCrud):
+    async def get_all_events(self, conn: AsyncConnection):
+        st = select(LaunchpadContractEvents).order_by(LaunchpadContractEvents.created_at.desc())
+        result = (await conn.execute(st)).all()
+        return result
+
     async def add_event(self, params: CreateLaunchpadEvent):
         values = params.dict()
         values["user_address"] = values["user_address"].lower()
--- app/services/analytics/jobs.py
+++ app/services/analytics/jobs.py
@@ -1,9 +1,13 @@
 import time
+from collections import defaultdict
 
+import pandas as pd
+import pygsheets
 from fastapi import Depends
+from pygsheets import PyGsheetsException
 
 from app.abi import LAUNCHPAD_CONTRACT_ADDRESS_ABI
-from app.base import logger
+from app.base import logger, engine
 from app.common import Command, CommandResult
 from app.crud.launchpad_events import LaunchpadContractEventsCrud
 from app.dependencies import get_launchpad_contract_events_crud
@@ -15,6 +19,37 @@ from app.services.web3_nodes import web3_node
 
 
 class ProcessLaunchpadContractEvents(Command):
+    async def get_all_events_and_save_to_gs(self, crud: LaunchpadContractEventsCrud):
+        if not settings.google_service_account_json:
+            logger.error("Process launchpad events: google_service_account_json not set")
+            return
+        data = defaultdict(list)
+        async with engine.begin() as conn:
+            events = await crud.get_all_events(conn)
+            for event in events:
+                data["#"].append(event.id)
+                data["event_type"].append(event.event_type.value)
+                data["user_address"].append(event.user_address)
+                data["token_address"].append(event.token_address)
+                data["contract_project_id"].append(event.contract_project_id)
+                data["amount"].append(event.extra.get("amount"))
+                data["tier"].append(event.extra.get("tier"))
+                data["txn_hash"].append(event.txn_hash)
+                data["block_number"].append(event.block_number)
+                data["created_at"].append(event.created_at)
+        dataframe = pd.DataFrame(data)
+
+        gc = pygsheets.authorize(
+            service_account_json=settings.google_service_account_json,
+        )
+        try:
+            sh = gc.open(settings.google_launchpad_events_report_filename)
+            sh[0].set_dataframe(dataframe, (1, 1))
+            filename = settings.google_launchpad_events_report_filename
+            logger.info(f"Saved launchpad events report to {filename}")
+        except PyGsheetsException as exc:
+            logger.error(f"Process launchpad events: gs exception: {str(exc)}")
+
     async def command(
         self,
         crud: LaunchpadContractEventsCrud = Depends(get_launchpad_contract_events_crud),
@@ -42,7 +77,7 @@ class ProcessLaunchpadContractEvents(Command):
             address=web3.to_checksum_address(settings.launchpad_contract_address),
             abi=LAUNCHPAD_CONTRACT_ADDRESS_ABI,
         )
-        n_user_registered, n_allocations = 0, 0
+        n_user_registered, n_allocations, new_events = 0, 0, 0
         try:
             while True:
                 from_block = last_checked_block + 1
@@ -58,6 +93,7 @@ class ProcessLaunchpadContractEvents(Command):
                     fromBlock=from_block, toBlock=to_block
                 )
                 n_user_registered += len(user_registered_events)
+                new_events += n_user_registered
                 for event in user_registered_events:
                     await crud.add_event(
                         params=CreateLaunchpadEvent(
@@ -75,6 +111,7 @@ class ProcessLaunchpadContractEvents(Command):
                     fromBlock=from_block, toBlock=to_block
                 )
                 n_allocations += len(tokens_allocations_events)
+                new_events += n_allocations
                 for event in tokens_allocations_events:
                     await crud.add_event(
                         params=CreateLaunchpadEvent(
@@ -99,5 +136,9 @@ class ProcessLaunchpadContractEvents(Command):
         except Exception as e:
             logger.error(f"Error processing events: {e}")
             return CommandResult(success=False, need_retry=True)
+        if new_events:
+            # todo: append only new events, not all events
+            # await self.get_all_events_and_save_to_gs(crud)
+            self.get_all_events_and_save_to_gs(crud)
 
         return CommandResult(success=True, need_retry=False)

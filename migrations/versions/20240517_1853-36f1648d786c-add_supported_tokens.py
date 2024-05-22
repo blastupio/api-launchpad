50fc209b22116ec6a5a65313c61feb796809b09b
Nikita <shamaevn@blastup.io>
2024-05-22 11:40:17 +0300

    fix migration

--- migrations/versions/20240517_1853-36f1648d786c-add_supported_tokens.py
+++ migrations/versions/20240517_1853-36f1648d786c-add_supported_tokens.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '36f1648d786c'
-down_revision: Union[str, None] = 'e1ca83d34ec1'
+down_revision: Union[str, None] = '931b35d41813'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 

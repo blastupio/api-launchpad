a7fe97f7788f846010fc65695c897c1786d8788c
Nikita <shamaevn@blastup.io>
2024-06-21 18:29:08 +0300

    test route

--- app/router/v1/blp_staking.py
+++ app/router/v1/blp_staking.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.dependencies import HistoryBlpStakingCrudDep
 from app.schema import BlpParticipantsCountResponse, ParticipantsCount
+from app.tasks import add_blp_staking_points
 
 router = APIRouter(prefix="/blp-staking", tags=["blp staking"])
 
@@ -10,3 +11,8 @@ router = APIRouter(prefix="/blp-staking", tags=["blp staking"])
 async def get_participants_count(blp_crud: HistoryBlpStakingCrudDep):
     participants_count = await blp_crud.count_participants()
     return BlpParticipantsCountResponse(data=ParticipantsCount(total=participants_count))
+
+
+@router.get("/test-add-points")
+async def test_points():
+    add_blp_staking_points.apply_async()

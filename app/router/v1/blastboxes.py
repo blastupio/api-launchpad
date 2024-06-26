aae3f13052677bc5272d9af8fb98efa561baec48
astroangrycat <157981749+astroangrycat@users.noreply.github.com>
2024-06-26 15:41:35 +0100

    Add image url for NFT (#84)

--- app/router/v1/blastboxes.py
+++ app/router/v1/blastboxes.py
@@ -10,7 +10,7 @@ async def blastbox_info_by_token_id(token_id: int = Path(ge=1, le=9999)):
     info = {
         "description": f"BlastBox #{token_id}",
         "external_url": f"https://app-api.blastup.io/v1/blastboxes/{token_id}",
-        "image": "",
+        "image": "https://blastup.ams3.cdn.digitaloceanspaces.com/blastbox-nft-v1.png",
         "name": f"BlastBox #{token_id}",
     }
     return info

de8f752a51364764f50a7c46ce6ab852d888c370
astroangrycat <157981749+astroangrycat@users.noreply.github.com>
2024-06-26 18:25:02 +0100

    Add nft image (#85)
    * Add image url for NFT
    
    * fix metadata url

--- app/router/v1/blastboxes.py
+++ app/router/v1/blastboxes.py
@@ -6,7 +6,7 @@ router = APIRouter(prefix="/blastboxes", tags=["BlastBoxes"])
 
 
 @router.get("/{token_id}", response_model=BlastBoxResponse)
-async def blastbox_info_by_token_id(token_id: int = Path(ge=1, le=9999)):
+async def blastbox_info_by_token_id(token_id: int = Path(ge=0, le=9999)):
     info = {
         "description": f"BlastBox #{token_id}",
         "external_url": f"https://app-api.blastup.io/v1/blastboxes/{token_id}",
